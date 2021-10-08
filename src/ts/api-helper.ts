26d621297d95efae8402a942b1b3b1bcbbf44c6e
Markaronin <jmarkroylance@gmail.com>
2021-10-08 17:23:54 -0600

    WIP trying to get the stupid lambdas to work

--- src/ts/api-helper.ts
+++ src/ts/api-helper.ts
@@ -1,9 +1,31 @@
+import { notUndefined } from "../../node_modules/@markaronin/jefferson-util/dist/index";
+
 interface UnsuccessfulRequest {
     success: false;
 }
 
+export function getCookie(cname: string): string | undefined {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return undefined;
+}
+
 export class APIHelper {
-    private static readonly baseUrl = "https://api.dnd-turn-order.markaronin.com";
+    private static readonly baseUrl = "https://m83obq436a.execute-api.us-east-1.amazonaws.com/prod";
+
+    public static getData(): Promise<UnsuccessfulRequest | { success: true; body: string }> {
+        return APIHelper.jsonGetRequest("data");
+    }
 
     // public static login(body: {usernameOrEmail: string, password: string}): Promise<UnsuccessfulRequest | {success: true, valid: false} | {success: true, valid: true, cookies: string[]}> {
     //     return APIHelper.jsonPostRequest("login", body)
@@ -13,15 +35,24 @@ export class APIHelper {
     //     return APIHelper.jsonPostRequest("register", body)
     // }
 
-    public static jsonPostRequest(url: string, body: unknown): Promise<any | UnsuccessfulRequest> {
+    private static jsonGetRequest(url: string): Promise<any | UnsuccessfulRequest> {
         return APIHelper.handleFetch(url, {
-            method: "POST",
+            method: "GET",
+        });
+    }
+
+    private static jsonPutRequest(url: string, body: unknown): Promise<any | UnsuccessfulRequest> {
+        return APIHelper.handleFetch(url, {
+            method: "PUT",
             body: JSON.stringify(body),
         });
     }
 
-    public static handleFetch(url: string, options: RequestInit): Promise<any | UnsuccessfulRequest> {
-        return fetch(`${APIHelper.baseUrl}/${url}`, options)
+    private static handleFetch(url: string, options: RequestInit): Promise<any | UnsuccessfulRequest> {
+        return fetch(`${APIHelper.baseUrl}/${url}`, {
+            ...options,
+            headers: { ...options.headers, authorization: notUndefined(getCookie("Auth")) },
+        })
             .then((response) => {
                 if (response.ok) {
                     return response.json();
